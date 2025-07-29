b6867126bcb3e432f4d3ae81a6f11fe47c1a808d
adarshshaji26@gmail.com <adarshshajiarackal17@gmail.com>
2025-07-29 16:43:37 +0530

    Added JQ

--- assets/style/main.js
+++ assets/style/main.js
@@ -7,37 +7,34 @@ $(document).ready(function (e) {
   }, function () {
     $('.contact-con-h').removeClass('slider');
   });
-  let mainSpan = document.querySelectorAll(".main-intro");
+  let mainSpan = document.querySelectorAll(".main-head");
   const introArray = Array('Web Developer','UIUX Designer','App Developer');
-    let index=0;
-  function displayContent(){
-  
-    if(index<introArray.length){
-      let h1 = document.createElement('h1');
-      let subDiv = $('.banner-intro-con').children('.main-head');
-    
-  
-      let data=introArray[index].split('');
-      console.log(data);
-    function appendData(){
-        let span = document.createElement('span');
-        var j =0;
-        if(j<data.length){
-           span.innerText = data[j];
-             $(h1).html(span);
-                 $(subDiv).html(h1);
-        }
-        setTimeout(()=>{
-          appendData();
-        },1000)
-    }
-      setTimeout(()=>{
-        displayContent();
-      },2000);
-      index=(index+1)%introArray.length;
-    }
-    appendData();
-  }
-  displayContent();
+  let i = 0;
+function displayContent() {
+  const mainSpan = $('.main-head'); // Use your target container
+    const heading = $('<h1></h1>').html(introArray[i]+"<span class='animation-bar'></span>");
+    mainSpan.html(heading);
+    animationBar();
 
+    i = (i + 1) % introArray.length; // Loop index
+
+}
+setInterval(displayContent,4000);
+  function animationBar(){
+      $('.animation-bar').animate({
+    width:"50%"
+  },4000,"swing",function(){
+    $(this).css({
+      "left":"70%",
+      "width":"5px",
+    });
+    setTimeout(()=>{
+      $('.animation-bar').animate({
+        left:"25%",
+
+      },4000);
+    })
+
+  })
+  }
 });
